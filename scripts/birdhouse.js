681f067125227c63c72055771c4e71fdce76e10a
bohdandrahan <bohdan.drahan@gmail.com>
2024-10-03 23:50:34 -0400

    add debug and add contacts

--- scripts/birdhouse.js
+++ scripts/birdhouse.js
@@ -1,39 +1,71 @@
 let smaller_window_size;
-let canvas_scale;
+let canvas_scale_x = 0.85;
+let canvas_scale_y = 0.7;
+let windows_width;
+let windows_height;
+
+let pixel_fracture = 1000; // divide window size on this var to get one unit of size;
 let unit;
 
+function preload() {
+    font = loadFont('/assets/inconsolata.otf');
+}
+
 function setup() {
+    textFont(font);
+    windows_width = window.innerWidth * canvas_scale_x;
+    windows_height = window.innerHeight * canvas_scale_y;
+
+    unit = Math.min(windows_width, windows_height) / pixel_fracture;
+
+
     let canvasContainer = document.getElementById('birdhouse-canvas');
-
-    canvas_scale = 0.8;
-    windows_width = window.innerWidth * canvas_scale;
-    windows_height = window.innerHeight * canvas_scale;
-
-    unit = Math.min(windows_width, windows_height) / 500
-
-
     createCanvas(windows_width, windows_height, WEBGL).parent(canvasContainer);
 
     background(200);
 
     cam = createCamera();
-    cam.setPosition(0, -400, 800);
-    cam.lookAt(10, 20, -30);
+    cam.setPosition(100, -400, 800);
+    cam.lookAt(0, 0, 0);
     translate(0, 0, 0)
+    textFont(font);
+    textSize(36);
 }
 
 function draw() {
-    background(200);
-    fill('blue');
-    circle(0, 0, 100, 10);
 
-
+    draw_debug()
     orbitControl(3, 3, 1);
+}
 
-    let x = 25 * sin(frameCount * 0.01) + 100;
+function draw_debug() {
+    background(200)
+    fill('deeppink');
+
+    translate(0, 0, 0)
+    fill('red');
+    circle(0, 0, 100 * unit);
+
+    fill('green')
+    sphere(10)
+
+    translate(500 * unit, 0, 0)
+    textSize(32);
+    fill('purple')
+    text('x', 0, 0);
+    translate(-500 * unit, 0, 0)
+
+    translate(0, 500 * unit, 0)
+    textSize(32);
+    fill('purple')
+    text('y', 0, 0);
+    translate(0, -500 * unit, 0)
+
+    translate(0, 0, 500 * unit)
+    textSize(32);
+    fill('purple')
+    text('z', 0, 0);
+
     normalMaterial();
-    console.log(cam.centerX, cam.centerY, cam.centerZ, cam)
-    // box(10)
-
-    // Draw the box.
+    translate(0, 0, 0)
 }
