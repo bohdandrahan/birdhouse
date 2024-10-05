e091f17dca76ab3246d4d4122724a439c31cdb3b
bohdandrahan <bohdan.drahan@gmail.com>
2024-10-05 13:55:17 -0400

    add some based for a birdhouse

--- scripts/birdhouse.js
+++ scripts/birdhouse.js
@@ -25,7 +25,7 @@ function setup() {
     background(200);
 
     cam = createCamera();
-    cam.setPosition(100, -400, 800);
+    cam.setPosition(0, 0, 3500 * unit);
     cam.lookAt(0, 0, 0);
     translate(0, 0, 0)
     textFont(font);
@@ -34,15 +34,67 @@ function setup() {
 
 function draw() {
 
-    draw_debug()
     orbitControl(3, 3, 1);
+    background(200)
+    draw_debug()
+
+    draw_bottom();
+    draw_front_wall();
+    draw_back_wall();
+    draw_right_wall()
+    draw_left_wall()
+    draw_right_foof()
+    draw_left_foof()
+}
+
+function draw_bottom() {
+    translate(0, 600 * unit, 0)
+    box(880 * unit, 100 * unit, 880 * unit, 0, 0)
+    translate(0, -600 * unit, 0)
+}
+
+function draw_front_wall() {
+    translate(0, 250 * unit, 300 * unit)
+    box(600 * unit, 600 * unit, 100 * unit, 0, 0)
+    translate(0, -250 * unit, -300 * unit)
+}
+function draw_back_wall() {
+    translate(0, 250 * unit, -300 * unit)
+    box(600 * unit, 600 * unit, 100 * unit, 0, 0)
+    translate(0, -250 * unit, 300 * unit)
+}
+function draw_right_wall() {
+    translate(350 * unit, 250 * unit, 0)
+    box(100 * unit, 600 * unit, 700 * unit, 0, 0)
+    translate(- 350 * unit, - 250 * unit, 0)
+}
+
+function draw_left_wall() {
+    translate(-350 * unit, 250 * unit, 0)
+    box(100 * unit, 600 * unit, 700 * unit, 0, 0)
+    translate(350 * unit, - 250 * unit, 0)
+}
+function draw_left_foof() {
+    rotate(QUARTER_PI);
+    translate(-350 * unit, 0, 0)
+    normalMaterial()
+    box(100 * unit, 800 * unit, 880 * unit, 0, 0)
+    translate(350 * unit, 0, 0)
+    rotate(-QUARTER_PI);
+}
+
+function draw_right_foof() {
+    rotate(-QUARTER_PI);
+    translate(350 * unit, 50 * unit, 0)
+    fill('black')
+    box(100 * unit, 700 * unit, 880 * unit, 0, 0)
+    translate(-350 * unit, -50 * unit, 0)
+    rotate(QUARTER_PI);
 }
 
 function draw_debug() {
-    background(200)
-    fill('deeppink');
 
-    translate(0, 0, 0)
+    fill('deeppink');
     fill('red');
     circle(0, 0, 100 * unit);
 
@@ -51,21 +103,23 @@ function draw_debug() {
 
     translate(500 * unit, 0, 0)
     textSize(32);
-    fill('purple')
-    text('x', 0, 0);
+    stroke(255, 204, 0);
+    strokeWeight(4);
+    fill('black')
+    text('moon - x axis', 0, 0);
     translate(-500 * unit, 0, 0)
 
     translate(0, 500 * unit, 0)
     textSize(32);
-    fill('purple')
-    text('y', 0, 0);
+    fill('gold')
+    text('sun - y axis', 0, 0);
     translate(0, -500 * unit, 0)
 
     translate(0, 0, 500 * unit)
     textSize(32);
-    fill('purple')
-    text('z', 0, 0);
+    fill('blue')
+    text('earth - z axis', 0, 0);
 
     normalMaterial();
-    translate(0, 0, 0)
+    translate(0, 0, -500 * unit)
 }
